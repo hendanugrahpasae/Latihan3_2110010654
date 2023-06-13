fc327013538540745443ff9545c97d26249c6cdf
hendanugrahpasae <hendanugrahpasae@gmail.com>
2023-06-13 16:42:32 +0800

    Membuat class Main dan menjalankan program

--- src/geometri/Main.java
+++ src/geometri/Main.java
@@ -2,21 +2,30 @@ package geometri;
 
 public class Main {
     public static void main(String[] args) {
-        Lingkaran lingkaran = new Lingkaran();
-        lingkaran.setRadius(7.5);
-
-        PersegiPanjang persegiPanjang = new PersegiPanjang();
-        persegiPanjang.setPanjang(4.0);
-        persegiPanjang.setLebar(6.0);
-
+        // Membuat objek Lingkaran
+        Lingkaran lingkaran = new Lingkaran(5.8);
+        
+        // Menghitung luas dan keliling Lingkaran
         double luasLingkaran = lingkaran.luas();
         double kelilingLingkaran = lingkaran.keliling();
+        
+        // Menampilkan hasil
+        System.out.println("Lingkaran:");
+        System.out.println("Luas: " + luasLingkaran);
+        System.out.println("Keliling: " + kelilingLingkaran);
+
+        System.out.println();
+
+        // Membuat objek PersegiPanjang
+        PersegiPanjang persegiPanjang = new PersegiPanjang(4, 6);
+        
+        // Menghitung luas dan keliling PersegiPanjang
         double luasPersegiPanjang = persegiPanjang.luas();
         double kelilingPersegiPanjang = persegiPanjang.keliling();
-
-        System.out.println("Luas Lingkaran: " + luasLingkaran);
-        System.out.println("Keliling Lingkaran: " + kelilingLingkaran);
-        System.out.println("Luas Persegi Panjang: " + luasPersegiPanjang);
-        System.out.println("Keliling Persegi Panjang: " + kelilingPersegiPanjang);
+        
+        // Menampilkan hasil
+        System.out.println("Persegi Panjang:");
+        System.out.println("Luas: " + luasPersegiPanjang);
+        System.out.println("Keliling: " + kelilingPersegiPanjang);
     }
 }
