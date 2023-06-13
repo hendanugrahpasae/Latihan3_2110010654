295955f1313431fd26b25e963e4f217cd410a4cf
hendanugrahpasae <hendanugrahpasae@gmail.com>
2023-06-13 16:23:39 +0800

    Membuat class Lingkaran dan PersegiPanjang

--- src/geometri/PersegiPanjang.java
+++ src/geometri/PersegiPanjang.java
@@ -4,28 +4,16 @@ public class PersegiPanjang {
     private double panjang;
     private double lebar;
 
-    public double getPanjang() {
-        return panjang;
-    }
-
-    public void setPanjang(double panjang) {
+    public PersegiPanjang(double panjang, double lebar) {
         this.panjang = panjang;
-    }
-
-    public double getLebar() {
-        return lebar;
-    }
-
-    public void setLebar(double lebar) {
         this.lebar = lebar;
     }
 
     public double luas() {
         return panjang * lebar;
-    
     }
 
     public double keliling() {
-        return 2*(panjang + lebar);
+        return 2 * (panjang + lebar);
     }
 }
